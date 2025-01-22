a7087dc646470bc172f203ea5f08904becd7254d
lamthanh-ebay <lamthanh.ebay@gmail.com>
2025-01-22 08:58:27 +0700

    Responsive Navbar OK

--- assets/js/scrips.js
+++ assets/js/scrips.js
@@ -133,3 +133,32 @@ function handleActiveMenu() {
         dropdown.onmouseleave = () => init();
     });
 }
+
+/**
+ * JS toggle
+ *
+ * Cách dùng:
+ * 1 - <button class="js-toggle" toggle-target="#box">Click</button>
+ * 2 - <div id="box">Content show/hide</div>
+ */
+window.addEventListener("template-loaded", initJsToggle);
+
+function initJsToggle() {
+    $$(".js-toggle").forEach((button) => {
+        const target = button.getAttribute("toggle-target");
+        if (!target) {
+            document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
+        }
+        button.onclick = () => {
+            if (!$(target)) {
+                return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
+            }
+            const isHidden = $(target).classList.contains("hide");
+
+            requestAnimationFrame(() => {
+                $(target).classList.toggle("hide", !isHidden);
+                $(target).classList.toggle("show", isHidden);
+            });
+        };
+    });
+}
